acc977cefdaace3353545cf5e971053602867242
dishankunnummal <bahiznalakath123@gmail.com>
2023-09-18 12:10:39 +0530

    some new line of codes are added

---
+++ lib/data/userinfo.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+enum TransactionType { outflow, inflow }
+
+enum ItemCategoryType { fashion, grocery, payments }
+
+class UserInfo {
+  final String name;
+  final String totalBalance;
+  final String inflow;
+  final String outflow;
+  final List<Transaction> transactions;
+
+  const UserInfo(
+      {required this.name,
+      required this.totalBalance,
+      required this.inflow,
+      required this.outflow,
+      required this.transactions});
+}
+
+class Transaction {
+  final ItemCategoryType categoryType;
+  final TransactionType transactionType;
+  final String itemCategoryName;
+
+  final String amount;
+  final String data;
+}
