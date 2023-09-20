5e00f969e87fec15f4628701629f0497a4ab8631
dishankunnummal <bahiznalakath123@gmail.com>
2023-09-20 20:33:51 +0530

    some new line of codes are added

--- lib/screens/home_screen_tab.dart
+++ lib/screens/home_screen_tab.dart
@@ -15,25 +15,63 @@ class HomeScreenTab extends StatelessWidget {
         child: Column(
           // crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(
+            const SizedBox(
               height: defaultSpacing * 4,
             ),
             ListTile(
-              leading: Image.asset("assets/images/avatar.jpeg",),
-              title: Text(" Hey ...name of user"),
-              trailing: Image.asset("assets/icons/bell."),),
-            Row(
+              leading: ClipRRect(
+                  borderRadius:
+                      BorderRadius.all(Radius.circular(defaultRadius)),
+                  child: Image.asset(
+                    "assets/images/avatar.jpeg",
+                  )),
+              title: const Text(" Hey ...name of user"),
+              trailing: Image.asset("assets/icons/bell.png"),
+            ),
+            const SizedBox(
+              height: defaultSpacing,
+            ),
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    "45000",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(fontSize: fontSizeHeading),
+                  ),
+                  const SizedBox(
+                    height: defaultSpacing,
+                  ),
+                  Text(
+                    "Total balance",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: defaultSpacing * 2,
+            ),
+            const Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               // mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(child: IncomeExpenseCard(expenseData: ExpenseData(
-                    "Income", "2000", Icons.arrow_upward_rounded),)),
+                Expanded(
+                    child: IncomeExpenseCard(
+                  expenseData:
+                      ExpenseData("Income", "2000", Icons.arrow_upward_rounded),
+                )),
                 SizedBox(
                   width: defaultSpacing,
                 ),
                 // Corrected widget name
-                Expanded(child: IncomeExpenseCard(expenseData: ExpenseData(
-                    "Expense", "9900", Icons.arrow_downward_rounded),)),
+                Expanded(
+                    child: IncomeExpenseCard(
+                  expenseData: ExpenseData(
+                      "Expense", "9900", Icons.arrow_downward_rounded),
+                )),
                 // Corrected widget name
               ],
             )
